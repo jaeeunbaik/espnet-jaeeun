2226c351bad869eff870ae8ba4f158fd9bf984f1
jaeeunbaik <jaeeunbaik53@gmail.com>
2024-10-16 16:00:33 +0900

    constitute again because of symbolic link

--- egs2/TEMPLATE/asr1/utils/data_prep.pl
+++ egs2/TEMPLATE/asr1/utils/data_prep.pl
@@ -8,7 +8,7 @@
 use open ':std', ':encoding(UTF-8)'; # Use UTF-8 encoding for all standard streams
 
 if (@ARGV != 3) {
-  print STDERR "Usage: $0 <path-to-commonvoice-corpus> <dataset> <valid-train|valid-dev|valid-test>\n";
+  print STDERR "Usage: $0 <path-to-voxpopuli-corpus> <dataset> <train|dev|test>\n";
   print STDERR "e.g. $0 /export/data/cv_corpus_v1 cv-valid-train valid-train\n";
   exit(1);
 }
@@ -19,14 +19,14 @@ if (length(`which ffmpeg`) == 0) {
   exit 1;
 }
 
-
+# db_base : /home/nas4/DB/VoxPopuli/transcribed_data/en
 ($db_base, $dataset, $out_dir) = @ARGV;
 mkdir data unless -d data;
 mkdir $out_dir unless -d $out_dir;
 
-open(CSV, "<", "$db_base/$dataset.tsv");
+open(CSV, "<", "$db_base/asr_$dataset.tsv");
 
-open(CSV, "<", "$db_base/$dataset.tsv") or die "cannot open dataset CSV file";
+open(CSV, "<", "$db_base/asr_$dataset.tsv") or die "cannot open dataset CSV file";
 open(SPKR,">", "$out_dir/utt2spk") or die "Could not open the output file $out_dir/utt2spk";
 open(GNDR,">", "$out_dir/utt2gender") or die "Could not open the output file $out_dir/utt2gender";
 open(TEXT,">", "$out_dir/text") or die "Could not open the output file $out_dir/text";
@@ -34,7 +34,7 @@ open(WAV,">", "$out_dir/wav.scp") or die "Could not open the output file $out_di
 my $header = <CSV>;
 while(<CSV>) {
   chomp;
-  ($spkr, $filepath, $text, $upvotes, $downvotes, $age, $gender, $accent) = split("\t", $_);
+  ($id, $raw_text, $normalized_text, $speaker_id, $split, $gender, $is_gold_transcript, $accent) = split("\t", $_);
   if ("$gender" eq "female") {
     $gender = "f";
   } else {
@@ -42,22 +42,23 @@ while(<CSV>) {
     $gender = "m";
   }
   $uttId = $filepath;
-  if (-z "$db_base/clips/$filepath") {
+  $year = substr($id, 0, 4);
+  if (-z "$db_base/transcribed_data/en/$year/$filepath") {
     print "null file $filepath\n";
     next;
   }
-  $uttId =~ s/\.mp3//g;
+  $uttId =~ s/\.ogg//g;
   $uttId =~ tr/\//-/;
   # speaker information should be suffix of the utterance Id
-  $uttId = "$spkr-$uttId";
-  $text = uc($text);
+  $uttId = "$speaker_id-$id";
+  $text = uc($normalized_text);
   if (index($text, "{") != -1 and index($text, "}" != -1)) {
     next;
   }
-  print TEXT "$uttId"," ","$text","\n";
-  print GNDR "$uttId"," ","$gender","\n";
-  print WAV "$uttId"," ffmpeg -i $db_base/clips/$filepath -f wav -ar 16000 -ab 16 -ac 1 - |\n";
-  print SPKR "$uttId"," $spkr","\n";
+  print TEXT "$id"," ","$normalized_text","\n";
+  print GNDR "$id"," ","$gender","\n";
+  print WAV "$id"," ffmpeg -i $db_base/transcribed_data/en/$year/$id.ogg -f wav -ar 16000 -ab 16 -ac 1 - |\n";
+  print SPKR "$id"," $speaker_id","\n";
 }
 close(SPKR) || die;
 close(TEXT) || die;
@@ -70,6 +71,6 @@ if (system(
   die "Error creating spk2utt file in directory $out_dir";
 }
 system("env LC_COLLATE=C utils/fix_data_dir.sh $out_dir");
-if (system("env LC_COLLATE=C utils/validate_data_dir.sh --non-print --no-feats $out_dir") != 0) {
+if (system("env LC_COLLATE=C utils/validate_data_dir.sh --no-spk-sort --non-print --no-feats $out_dir") != 0) {
   die "Error validating directory $out_dir";
 }
